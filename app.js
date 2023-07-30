699b2934dffc2abc8b6f37c9447f8a7293b8d683
DESKTOP-3LC3JT7\Zahid Hasan <zahidcse4960@gmail.com>
2023-07-30 18:51:01 +0600

    added routes, model, services & controller

--- app.js
+++ app.js
@@ -2,9 +2,16 @@ require('dotenv').config();
 
 const express = require('express')
 const port = process.env.PORT
+const testRoute = require('./routes/testRoute')
 
 const app = express()
 
+app.get('/', (req, res) => {
+    res.send('Hello from leave tracker backend!')
+})
+
+app.use('/api/test', testRoute)
+
 app.listen(port, () => {
-    console.log(`Server is running on localhost:${port}`)
+    console.log(`Server is running on http://localhost:${port}`)
 })
