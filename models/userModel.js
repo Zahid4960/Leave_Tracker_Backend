8464693b855b7d3ecaef1c0970937eb473942621
DESKTOP-3LC3JT7\Zahid Hasan <zahidcse4960@gmail.com>
2023-09-21 23:40:13 +0600

    updated model writing structure to increase readability & write swagger definition for userlogin api

--- models/userModel.js
+++ models/userModel.js
@@ -4,49 +4,20 @@ const addressSchema = require('../schemas/addressSchema')
 
 
 const userSchema = new Schema({
-  firstName: {
-    type: String,
-    required: true
-  },
-  lastName: {
-    type: String,
-    default: ''
-  },
-  userName: {
-    type: String,
-    default: ''
-  },
-  email: {
-    type: String,
-    required: true,
-    unique: true,
-  },
-  emailVerifiedAt: {
-    type: Date
-  },
-  recoveryEmail: {
-    type: String,
-  },
+  firstName: { type: String, required: true },
+  lastName: { type: String },
+  userName: { type: String },
+  email: { type: String, required: true, unique: true },
+  emailVerifiedAt: { type: Date },
+  recoveryEmail: { type: String },
   address: [addressSchema],
-  dop: {
-    type: String
-  },
-  password: {
-    type: String,
-    required: true
-  },
-  isAccountActivatedByOwner: {
-    type: Boolean,
-    default: false
-  },
-  otp: {
-    typr: Number
-  },
-  userType: {
-    type: String,
-    enum: ['Company', 'User', 'Admin'],
-    default: 'User'
-  },
+  dop: { type: String },
+  password: { type: String, required: true },
+  isAccountActivatedByOwner: { type: Boolean, default: false },
+  otp: { type: Number },
+  token: { type: String, required: true },
+  isRemember: { type: Boolean, default: false },
+  userType: { type: String, enum: ['Company', 'User', 'Admin'], default: 'User' },
   ...commonSchema.obj
 });
 
