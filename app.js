b33549d823a20738f7613f554c336d9abf063621
DESKTOP-3LC3JT7\Zahid Hasan <zahidcse4960@gmail.com>
2023-09-21 20:58:50 +0600

    Integrate swagger to this project & update readme file

--- app.js
+++ app.js
@@ -1,7 +1,13 @@
 require('dotenv').config()
 
 const express = require('express')
+const swaggerUi = require('swagger-ui-express');
+const jsyaml = require('js-yaml');
+const fs = require('fs');
+const path = require('path');
+
 const port = process.env.PORT
+
 const testRoute = require('./routers/test.router')
 const dbConnection = require('./config/dbConfig')
 
@@ -9,6 +15,12 @@ const dbConnection = require('./config/dbConfig')
 const app = express()
 
 dbConnection()
+
+// Load YAML file
+const swaggerDocument = jsyaml.load(fs.readFileSync(path.join(__dirname, './swagger/index.yaml'), 'utf8'))
+
+// Swagger setup
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 app.get('/', (req, res) => {
     res.send('Hello from leave tracker backend!')
